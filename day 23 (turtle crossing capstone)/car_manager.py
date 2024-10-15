a0dcad90f2843e42e4ba6764a8a1d636b0407fd7
EF361 <aliciagoh0704@gmail.com>
2024-10-15 22:55:18 +0800

    create car functionality

--- day 23 (turtle crossing capstone)/car_manager.py
+++ day 23 (turtle crossing capstone)/car_manager.py
@@ -1,7 +1,26 @@
+from turtle import Turtle as t
+import random as r
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(t):
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = r.randint(1, 6)
+        if random_chance == 1:
+            new_car = t("square")
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.penup()
+            new_car.color(r.choice(COLORS))
+            random_y = r.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
