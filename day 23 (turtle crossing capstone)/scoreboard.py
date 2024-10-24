fa68666a3e497996c1a6ff9447f0baba53ce0ee1
EF361 <aliciagoh0704@gmail.com>
2024-10-24 17:41:23 +0800

    update scoreboard

--- day 23 (turtle crossing capstone)/scoreboard.py
+++ day 23 (turtle crossing capstone)/scoreboard.py
@@ -1,5 +1,25 @@
+from turtle import Turtle as t
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(t):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def level_up(self):
+        self.clear()
+        self.level += 1
+        self.update_scoreboard()
+
+    def end_game(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
