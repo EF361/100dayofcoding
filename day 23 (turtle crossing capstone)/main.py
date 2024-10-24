f187c155831db7074c78b20f2f443772ee0f5c4b
EF361 <aliciagoh0704@gmail.com>
2024-10-24 17:41:58 +0800

    update main

--- day 23 (turtle crossing capstone)/main.py
+++ day 23 (turtle crossing capstone)/main.py
@@ -12,6 +12,7 @@ screen.tracer(0)
 # define object
 tim = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(tim.move, "Up")
@@ -23,3 +24,17 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    # detects collision with car
+    for car in car_manager.all_cars:
+        if car.distance(tim) < 20:
+            game_is_on = False
+            scoreboard.end_game()
+
+    # detect successful crossing
+    if tim.is_at_finish_line():
+        tim.go_to_start()
+        scoreboard.level_up()
+
+
+screen.exitonclick()
