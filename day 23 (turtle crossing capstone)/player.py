4d232ffc050673bba0ba7b04440a709cfebb7b6f
EF361 <aliciagoh0704@gmail.com>
2024-10-24 17:41:46 +0800

    update player

--- day 23 (turtle crossing capstone)/player.py
+++ day 23 (turtle crossing capstone)/player.py
@@ -11,7 +11,16 @@ class Player(t):
         self.shape("turtle")
         self.setheading(90)
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
 
     def move(self):
         self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
