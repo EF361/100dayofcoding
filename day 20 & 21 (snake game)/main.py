c401b77187c6e357d76b6379e6c2ebb2031dab99
EF361 <aliciagoh0704@gmail.com>
2024-10-29 20:15:15 +0800

    update main

--- day 20 & 21 (snake game)/main.py
+++ day 20 & 21 (snake game)/main.py
@@ -42,13 +42,14 @@ while game_on:
         or snake.head.ycor() < -295
         or snake.head.ycor() > 295
     ):
-        game_on = False
-        display.game_over()
+        display.reset()
+        snake.reset()
 
     # detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_on = False
-            display.game_over()
+            display.reset()
+            snake.reset()
+
 
 screen.exitonclick()
