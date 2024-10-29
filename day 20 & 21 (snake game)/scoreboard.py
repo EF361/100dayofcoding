03e32c40995ed7762d6de06ac91a4680fc35b883
EF361 <aliciagoh0704@gmail.com>
2024-10-29 20:36:35 +0800

    update scoreboard to display the high score

--- day 20 & 21 (snake game)/scoreboard.py
+++ day 20 & 21 (snake game)/scoreboard.py
@@ -8,7 +8,9 @@ class Scoreboard(T):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+
         self.color("white")
         self.penup()
         self.goto(-10, 280)
@@ -31,5 +33,7 @@ class Scoreboard(T):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.high_score))
         self.score = 0
         self.update_score()
